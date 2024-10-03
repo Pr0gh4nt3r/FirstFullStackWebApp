203345196e801d7d85b8c3c16d1367225013563b
Pr0gh4nt3r <github.shortly055@passmail.com>
2024-10-03 05:22:42 +0200

    feat(server): create CRUD api
    - add POST and GET
    - add model for schema

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,38 @@
 import express, { json } from "express";
+import * as mongoose from "mongoose";
+
+import { UserDataModel } from "./models/userData.models.js";
 
 const port = 1337;
 const app = express();
 
 app.use(json());
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}!`);
+app.post('/api/userdata', async (req, res) => {
+    try {
+        const userData = await UserDataModel.create(req.body);
+        res.status(200).json(userData);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+app.get('/api/userdata', async (req, res) => {
+    try {
+        const userData = await UserDataModel.find(req.body);
+        res.status(200).json(userData);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+mongoose.connect("mongodb://172.24.48.1:27017/test")
+.then(() => {
+    console.log("Database connection successfully established!");
+    app.listen(port, () => {
+        console.log(`Listening on port ${port}!`);
+    });
+})
+.catch((err) => {
+    console.error("Database connection failed!", err);
 });
