c9780613f99dd4fba3e3c0fe5a29ba4722f41f06
Pr0gh4nt3r <github.shortly055@passmail.com>
2025-05-07 01:08:11 +0200

    feat(server): add function
    - getUserWithAddress

--- Server/src/Controllers/user.controller.ts
+++ Server/src/Controllers/user.controller.ts
@@ -86,14 +86,14 @@ export const getUserWithPersonalData = async (req: Request, res: Response) => {
             "personalData.phones": "$phones", // Ersetze das Array mit den neuen Nummern
           },
         },
-        // Hier das typeInfo-Feld löschen:
-        {
-          $unset: ["personalData.phones.typeInfo", "phones"],
-        },
         // 9) Säubere Hilfsfelder
         {
           $project: {
+            _id: 0,
             phones: 0,
+            "personalData._id": 0,
+            "personalData.phones.typeInfo": 0,
+            "personalData.addresses": 0,
           },
         },
       ]); // Typisiere das Ergebnis als Array von IUserDocument
@@ -111,16 +111,128 @@ export const getUserWithAddresses = async (req: Request, res: Response) => {
   try {
     const userWithAddresses: IUserDocument[] | null = await UserModel.aggregate(
       [
+        // 1) Benutzer finden
         {
           $match: { _id: new mongoose.Types.ObjectId(id) }, // Finde den Benutzer basierend auf der User-ID
         },
+        // 2) personalData einbinden (einfacher Lookup, kein Unwind)
         {
           $lookup: {
-            // Verknüpfe die Adressen basierend auf den ObjectId's in personalData.addresses
-            from: "addresses", // Die Collection, aus der wir die Adressen beziehen
-            localField: "personalData.addresses", // Das Feld in der User-Collection, das die Adressen-IDs enthält
-            foreignField: "_id", // Das Feld in der Address-Collection, das die ObjectId's speichert
-            as: "personalData.addresses", // Wie das Ergebnis im Enddokument genannt wird
+            from: "personalData",
+            localField: "personalData",
+            foreignField: "_id",
+            as: "personalData",
+          },
+        },
+        // Da wir aber personalData nur einmal erwarten, packen wir es als Objekt
+        {
+          $addFields: { personalData: { $arrayElemAt: ["$personalData", 0] } },
+        },
+        // 3) Phones auflösen (wie gehabt)
+        {
+          $unwind: {
+            path: "$personalData.phones",
+            preserveNullAndEmptyArrays: true,
+          },
+        },
+        {
+          $lookup: {
+            from: "numberTypes",
+            localField: "personalData.phones.type",
+            foreignField: "_id",
+            as: "personalData.phones.typeInfo",
+          },
+        },
+        {
+          $addFields: {
+            "personalData.phones.type": {
+              $arrayElemAt: ["$personalData.phones.typeInfo.description", 0],
+            },
+          },
+        },
+        {
+          $group: {
+            _id: "$_id",
+            // Felder vor personalData behalten
+            userName: { $first: "$userName" },
+            email: { $first: "$email" },
+            password: { $first: "$password" },
+            confirmed: { $first: "$confirmed" },
+            // personalData bis hierher einmal nehmen
+            personalData: { $first: "$personalData" },
+            // alle phones sammeln
+            phones: { $push: "$personalData.phones" },
+          },
+        },
+        { $addFields: { "personalData.phones": "$phones" } },
+        // 4) Addresses auflösen und type ersetzen
+        {
+          $unwind: {
+            path: "$personalData.addresses",
+            preserveNullAndEmptyArrays: true,
+          },
+        },
+        {
+          $lookup: {
+            from: "addresses",
+            localField: "personalData.addresses",
+            foreignField: "_id",
+            as: "addressObj",
+          },
+        },
+        {
+          $addFields: {
+            personalData: {
+              $mergeObjects: [
+                "$personalData",
+                { addresses: { $arrayElemAt: ["$addressObj", 0] } },
+              ],
+            },
+          },
+        },
+        // 5) Typ auflösen für jede Adresse
+        {
+          $lookup: {
+            from: "addressTypes",
+            localField: "personalData.addresses.type",
+            foreignField: "_id",
+            as: "typeInfo",
+          },
+        },
+        {
+          $addFields: {
+            "personalData.addresses.type": {
+              $arrayElemAt: ["$typeInfo.description", 0],
+            },
+          },
+        },
+        // 6) Addresses wieder zum Array gruppieren
+        {
+          $group: {
+            _id: "$_id",
+            userName: { $first: "$userName" },
+            email: { $first: "$email" },
+            password: { $first: "$password" },
+            confirmed: { $first: "$confirmed" },
+            personalData: { $first: "$personalData" },
+            addresses: { $push: "$personalData.addresses" },
+          },
+        },
+        { $addFields: { "personalData.addresses": "$addresses" } },
+        // 7) Säubere Hilfsfelder
+        {
+          $project: {
+            _id: 0,
+            phones: 0,
+            typeInfo: 0,
+            addressObj: 0,
+            addresses: 0,
+            "personalData._id": 0,
+            "personalData.phones.typeInfo": 0,
+            "personalData.addresses._id": 0,
+            "personalData.addresses.createdAt": 0,
+            "personalData.addresses.updatedAt": 0,
+            "personalData.addresses.__v": 0,
           },
         },
       ]
