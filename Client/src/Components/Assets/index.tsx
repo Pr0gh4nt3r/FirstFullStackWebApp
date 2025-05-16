16c5e7fbae3837cdee50e508e891dc17004747d9
Pr0gh4nt3r <github.shortly055@passmail.com>
2025-05-16 03:29:41 +0200

    refactor(client): #22 - add global styling

---
+++ Client/src/Components/Assets/index.tsx
@@ -0,0 +1,21 @@
+// src/Assets/index.tsx
+
+import { ReactComponent as UserIcon } from "./user.svg";
+import { ReactComponent as EmailIcon } from "./email.svg";
+import { ReactComponent as PasswordIcon } from "./password.svg";
+import { ReactComponent as SettingsIcon } from "./settings.svg";
+import { ReactComponent as RightIcon } from "./right.svg";
+import { ReactComponent as LogoutIcon } from "./logout.svg";
+import { ReactComponent as HomeIcon } from "./home.svg";
+import { ReactComponent as SecurityIcon } from "./security.svg";
+
+export {
+  UserIcon,
+  EmailIcon,
+  PasswordIcon,
+  SettingsIcon,
+  RightIcon,
+  LogoutIcon,
+  HomeIcon,
+  SecurityIcon,
+};
